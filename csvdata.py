ff3594f9e9777ddb257e9070120997dbd09c51c3
Jay_Project <jaymisal032@gmail.com>
2022-04-10 21:26:43 +0530

    commits done to the code

--- csvdata.py
+++ csvdata.py
@@ -10,16 +10,16 @@ la = [1.4,5,34.54,'jay','avengers']
 fn = Abc()
 la = fn.func(la,6,"hello")
 
-for i in la:
-    if(isinstance(i,list) or isinstance(i,tuple) or isinstance(i,set)):
-        for j in i:
-            print(j,end=" ,")
-    else:
-        print(i)
+# for i in la:
+#     if(isinstance(i,list) or isinstance(i,tuple) or isinstance(i,set)):
+#         for j in i:
+#             print(j,end=" ,")
+#     else:
+#         print(i)
         
-for i in la:
-    if(isinstance(i,list) or isinstance(i,tuple) or isinstance(i,set)):
-        for j in i:
-            print(j,end=" ,")
-    else:
-        print(i)
+# for i in la:
+#     if(isinstance(i,list) or isinstance(i,tuple) or isinstance(i,set)):
+#         for j in i:
+#             print(j,end=" ,")
+#     else:
+#         print(i)
