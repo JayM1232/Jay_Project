8cebf945833a866ccb0d904daf5a306be2682080
Jay_Project <jaymisal032@gmail.com>
2022-04-10 21:13:09 +0530

    changes done as specified

--- csvdata.py
+++ csvdata.py
@@ -17,3 +17,9 @@ for i in la:
     else:
         print(i)
         
+for i in la:
+    if(isinstance(i,list) or isinstance(i,tuple) or isinstance(i,set)):
+        for j in i:
+            print(j,end=" ,")
+    else:
+        print(i)
